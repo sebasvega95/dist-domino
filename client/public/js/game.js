a8f53a0d4e222eb514394dbf85ba1ada28006e30
sebasvega95 <sebas060495@gmail.com>
2016-11-24 18:07:11 -0500

    Drawing game in spiral

--- client/public/js/game.js
+++ client/public/js/game.js
@@ -1,8 +1,10 @@
 /* eslint no-unused-vars: ["error", { "vars": "local" }] */
 
 /* ----- Constants ----- */
-const dominoSize = 45;
+const dominoSize = 30;
 const dominoDot = dominoSize * 0.1;
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
 
 /* ----- Domino drawing -----*/
 
@@ -25,6 +27,7 @@ function dotsOfPiece(n, x, y) {
 
   switch (n) {
     case 0:
+      dots = [];
       break;
     case 1:
       dots = [middle];
@@ -84,7 +87,6 @@ function drawDomino(domino, x, y, horizontal = true) {
   first.forEach((dots) => {
     ellipse(dots.x, dots.y, dominoDot);
   });
-
   second.forEach((dots) => {
     ellipse(dots.x, dots.y, dominoDot);
   });
@@ -98,20 +100,99 @@ function drawDomino(domino, x, y, horizontal = true) {
 function setup() {
   let canvas = createCanvas(600, 400);
   canvas.parent('main-container');
+  const dominos = [
+    [0, 0],
+    [0, 1], [1, 1],
+    [0, 2], [1, 2], [2, 2],
+    [0, 3], [1, 3], [2, 3], [3, 3],
+    [0, 4], [1, 4], [2, 4], [3, 4], [4, 4],
+    [0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5],
+    [0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6],
+  ];
+  const delta = 5;
+
+  clear();
+  let x = 10;
+  let limInfX = x;
+  let limSupX = width;
+  let y = 10;
+  let limInfY = 2 * y + dominoSize;
+  let limSupY = height;
+  let dir = 0;
+  let horizontal;
+
+  dominos.forEach((piece) => {
+    horizontal = dir == 0 || dir == 2;
+
+    // if (piece[0] == piece[1]) {
+    //   let drawDir = dir;
+    //   switch (dir) {
+    //     case 0: case 1:
+    //       drawDir += 2;
+    //       break;
+    //     case 2:
+    //       drawDir++;
+    //       break;
+    //     case 3:
+    //       drawDir--;
+    //       break;
+    //   }
+    //
+    //   drawDomino(piece, x + dx[drawDir] * dominoSize,
+    //     y + dy[drawDir] * dominoSize, !horizontal);
+    //   x += dx[dir] * (dominoSize + delta);
+    //   y += dy[dir] * (dominoSize + delta);
+    // } else {
+    //   drawDomino(piece, x, y, horizontal);
+    //   x += dx[dir] * (2 * dominoSize + delta);
+    //   y += dy[dir] * (2 * dominoSize + delta);
+    // }
+
+    drawDomino(piece, x, y, horizontal);
+    x += dx[dir] * (2 * dominoSize + delta);
+    y += dy[dir] * (2 * dominoSize + delta);
+
+    switch (dir) {
+      case 0:
+        if (x + 2 * dominoSize >= limSupX) {
+          x -= dominoSize + delta;
+          limSupX = x + dominoSize / 2;
+          y += dominoSize + delta;
+          dir++;
+        }
+        break;
+      case 1:
+        if (y + 2 * dominoSize >= limSupY) {
+          y -= dominoSize + delta;
+          limSupY = y + dominoSize / 2;
+          x -= 2 * dominoSize + delta;
+          dir++;
+        }
+        break;
+      case 2:
+        if (x <= limInfX) {
+          x += 2 * dominoSize + delta;
+          limInfX = x + dominoSize / 2;
+          y -= 2 * dominoSize + delta;
+          dir++;
+        }
+        break;
+      case 3:
+        if (y <= limInfY) {
+          y += 2 * dominoSize + delta;
+          limInfY = y + dominoSize / 2;
+          x += dominoSize + delta;
+          dir++;
+        }
+        break;
+    }
+
+    dir %= 4;
+  });
 }
 
 /**
  * Draws using p5
  */
 function draw() {
-  const dominos = [[1, 2], [3, 4], [5, 6]];
-  const delta = 5;
-
-  clear();
-  let x = 10;
-  dominos.forEach((piece) => {
-    // drawDomino(piece, x, height / 2, false);
-    drawDomino(piece, width / 2, x, false);
-    x += 2 * dominoSize + delta;
-  });
 }
